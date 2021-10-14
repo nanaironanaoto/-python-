6088d58e6dc867e985a2ade631869450362e6a29
MYCOMPUTER\yoshikawa <yoshikawa-n@cin-group.com>
2021-10-14 10:22:19 +0900

    bs4 setup

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Hello, World")
+
+from bs4 import BeautifulSoup
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.keys import Keys
+
+options = ChromeOptions()
+options.add_argument('--headless')
+driver = Chrome(options=options)
